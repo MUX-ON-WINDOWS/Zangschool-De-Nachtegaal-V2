7a7b838de56b778aa98b3c4143d4e423c56c8ca0
Max <maxnouts@gmail.com>
2023-02-26 16:28:29 +0100

    Added prijzen page and updated main.css

--- src/views/Prijzen.vue
+++ src/views/Prijzen.vue
@@ -1,8 +1,23 @@
-<script setup>
-</script>
-
 <template>
-  <main>
-    Hoi dit is de prijzen pagina
-  </main>
+  <div class="containerZanglessen">
+    <div class="prijsOffer">
+      <img class="logo-prijzen" src="../assets/images/zangvogel.png" alt="logo">
+      <h1>Cadeaubon zangles</h1>
+      <p>Vera houdt van klassieke muziek en zingt als sopraan mee bij het Brabants koor La Belle Trottoir. Bekijk hier deze Youtube video om haar in actie te zien.</p>
+
+      <button class="winkelwagenButton">Nu kopen</button>
+    </div>
+    <div class="prijsOffer">
+      <img class="logo-prijzen" src="../assets/images/zangvogel.png" alt="logo">
+      <h1>oefenles (online)</h1>
+      <p>Karel is een echte rapper in hart en nieren en leert je rappen als Extince of Snoop Dogg. Bekijk hier deze Youtube video om Karel in actie te zien.</p>
+      <button class="winkelwagenButton">Nu kopen</button>
+    </div>
+    <div class="prijsOffer">
+      <img class="logo-prijzen" src="../assets/images/zangvogel.png" alt="logo">
+      <h1>Oefenles (met coach)</h1>
+      <p>Ben jij ook zo fan van Oostenrijkse tiroler muziek? Wil jij leren jodelen op een mooie groene berg? Neem dan lessen bij Berta Buizerd!</p>
+      <button class="winkelwagenButton">Nu kopen</button>
+    </div>
+  </div>
 </template>

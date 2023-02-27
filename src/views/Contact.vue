f8c4cecfc89952a9063bd04b1b3d982e921ba88b
Max <maxnouts@gmail.com>
2023-02-27 14:51:15 +0100

    added grid to contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -13,15 +13,21 @@
 .container-contact {
   display: grid;
   place-items: center;
+  grid-template-columns: 0.5fr 1fr 0.2fr 1fr 0.5fr;
+  grid-template-rows: repeat(5, 1fr);
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
 }
 .container-googleMaps {
-  width: 45%;
-  height: 350px;
+  width: 100%;
+  height: 100%;
+  grid-area: 2 / 2 / 5 / 3;
   background-color: #C37E23;
 }
 .container-contactPage {
-  width: 45%;
-  height: 350px;
+  width: 100%;
+  height: 400px;
+  grid-area: 2 / 4 / 5 / 5;
   background-color: #C37E23;
 }
 </style>
